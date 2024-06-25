a8eee7d2e2e8678eb85995f27e18328b760eca6d
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-06-25 14:02:07 +0900

    Create 짝수의 합.js

---
+++ ALGORITHM/짝수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    if(n<=0||n>1000) return answer;
+    for(var i = 0; i<=n;i++){
+        if(i%2==0){
+            answer+=i;
+        }
+    }
+    return answer;
+}
