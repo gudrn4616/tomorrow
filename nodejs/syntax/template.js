6c00b6929161420b39c8326ae7b5abdec37cfaa9
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-06-26 16:32:52 +0900

    6.26

---
+++ nodejs/syntax/template.js
@@ -0,0 +1,11 @@
+var letter = 'Lorem ipsum dolor sit amet,\n\
+ consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\
+ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n\
+ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \n\
+ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
+
+var name = 'asdf';
+
+var letter2 = `${name} Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+console.log(letter2);
