7de940c4d4f8206cef8aaa0dabbc0a78ef43bb2f
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-06-27 14:41:26 +0900

    6.27

--- ALGORITHM/가장 가까운 같은 글자.js
+++ ALGORITHM/가장 가까운 같은 글자.js
@@ -9,8 +9,8 @@ function solution(s) {
     for(var i = 0; i<s.length ;i++){
         var index = s.charCodeAt(i) - 'a'.charCodeAt(0);
         
-        if(Alp[index] == -1) answer.push(-1);
-        else answer.push(i-Alp[index]);
+        if(Alp[index] == -1)    answer.push(-1);
+        else                    answer.push(i-Alp[index]);
             
         Alp[index] = i;
     }
