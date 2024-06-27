b366a2d28afd73c245dd9d70a93b749a6f774d93
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-06-27 14:35:03 +0900

    6.27

---
+++ ALGORITHM/가장 가까운 같은 글자.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var Alp=new Array(26);
+    var answer = [];
+    
+    for(var i = 0; i<26;i++){
+        Alp[i]=-1;
+    }
+    
+    for(var i = 0; i<s.length ;i++){
+        var index = s.charCodeAt(i) - 'a'.charCodeAt(0);
+        
+        if(Alp[index] == -1) answer.push(-1);
+        else answer.push(i-Alp[index]);
+            
+        Alp[index] = i;
+    }
+    
+    return answer;
+}
